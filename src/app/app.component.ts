024f21966e38312801e1dea852064e1dfe8d81f9
JAFB537 <jafb537@gmail.com>
2024-07-20 07:59:45 -0600

    Update App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, CommonModule, HeaderComponent, ContentComponent],
+  templateUrl: `./app.component.html`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-17-app';
+  greet() {
+    this.state = !this.state
+  }
+
+  title = 'Angular 17 App';
+  user = 'Jose Flores'
+  state = false
 }
